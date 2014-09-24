44fd758f613d4ef9ad3099f0e2e1cc032d6cf546
Kattya <kattya.cuevas@gmail.com>
2014-09-24 10:27:42 -0500

    Agregar inflecciones

--- 2014-10/02-rails/devflix/config/initializers/inflections.rb
+++ 2014-10/02-rails/devflix/config/initializers/inflections.rb
@@ -3,6 +3,13 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
+ActiveSupport::Inflector.inflections.clear
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural /([rlnd])([A-Z]|_|$)/, '\1es\2'
+  inflect.plural /([aeiou])([A-Z]|_|$)/, '\1s\2'
+  inflect.singular /([aeiou])s([A-Z]|_|$)/, '\1\2'
+  inflect.singular /([rlnd])es([A-Z]|_|$)/, '\1\2'
+end
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
