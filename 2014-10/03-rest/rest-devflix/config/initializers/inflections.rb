d64d376ff4c1f304def5ed9ad11143d18a6001a2
Kattya <kattya.cuevas@gmail.com>
2014-09-30 17:23:57 -0500

    Agregar inflecciones

--- 2014-10/03-rest/rest-devflix/config/initializers/inflections.rb
+++ 2014-10/03-rest/rest-devflix/config/initializers/inflections.rb
@@ -1,5 +1,11 @@
 # Be sure to restart your server when you modify this file.
-
+ActiveSupport::Inflector.inflections.clear
+ActiveSupport::Inflector.inflections do |inflect|
+ inflect.plural /([rlnd])([A-Z]|_|$)/, '\1es\2'
+ inflect.plural /([aeiou])([A-Z]|_|$)/, '\1s\2'
+ inflect.singular /([aeiou])s([A-Z]|_|$)/, '\1\2'
+ inflect.singular /([rlnd])es([A-Z]|_|$)/, '\1\2'
+end
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
