2033cf4f537cb7f05608089bcd106e92c2a82208
Kattya <kattya.cuevas@gmail.com>
2014-09-30 17:47:50 -0500

    Agregar método index

--- 2014-10/03-rest/rest-devflix/app/controllers/series_controller.rb
+++ 2014-10/03-rest/rest-devflix/app/controllers/series_controller.rb
@@ -1,2 +1,6 @@
 class SeriesController < ApplicationController
+  def index
+    @series = Serie.all
+    render json: @series
+  end
 end
--- 2014-10/03-rest/rest-devflix/spec/controllers/series_controller_spec.rb
+++ 2014-10/03-rest/rest-devflix/spec/controllers/series_controller_spec.rb
@@ -1,3 +1,11 @@
 require 'rails_helper'
 describe SeriesController do
+  it 'GET index' do
+    Serie.create(nombre: 'Bates Motel')
+    Serie.create(nombre: 'The Following')
+    get :index
+    expect(response.status).to eq 200
+    json = JSON.parse(response.body)
+    expect(json.length).to eq 2
+  end
 end
