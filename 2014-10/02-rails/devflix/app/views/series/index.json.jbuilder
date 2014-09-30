735c5bc7db5aa8f3d1ccbf82a8375bc12c9140d8
Kattya <kattya.cuevas@gmail.com>
2014-09-30 17:01:26 -0500

    Quitar proyecto fallido

--- 2014-10/02-rails/devflix/app/views/series/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@series) do |serie|
-  json.extract! serie, :id, :nombre, :temporadas, :descripcion, :imagen, :en_transmision
-  json.url serie_url(serie, format: :json)
-end
--- 2014-10/02-rails/devflix/db/seeds.rb
+++
@@ -1,8 +0,0 @@
-puts "Seed Serie"
-Serie.delete_all
-
-Serie.create( nombre: "Bates Motel",
-              temporadas: 2,
-              descripcion: "Bates Motel retrata la vida de un joven de aspecto frágil llamado Norman Bates y de su posesiva madre Norma, unos años antes de los acontecimientos narrados en Psicosis.",
-              imagen: "assets/images/bates-motel.jpg",
-              en_transmision: true)
