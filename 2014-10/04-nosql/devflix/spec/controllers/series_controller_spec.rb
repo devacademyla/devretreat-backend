d67b4cfe91827e9b1f7850a35b356d541aa3523f
Kattya <kattya.cuevas@gmail.com>
2014-10-09 12:27:05 -0500

    9.5 TDD index REFACTOR

--- 2014-10/04-nosql/devflix/spec/controllers/series_controller_spec.rb
+++ 2014-10/04-nosql/devflix/spec/controllers/series_controller_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 describe SeriesController do
+  before do
+    Serie.delete_all
+  end
   describe 'GET index' do
     it 'ok' do
       get :index
-      expect(response.status).to eq 200
+      expect(response).to have_http_status(:ok)
     end
     it 'render json' do
       Serie.create(nombre: 'Bates Motel')
       get :index
-      expect(response.status).to eq 200
+      expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json.size).to eq 1
     end
