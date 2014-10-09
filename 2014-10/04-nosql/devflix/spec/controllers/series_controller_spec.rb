95a485ec7c331c82d5537d9123c3bba3840428ec
Kattya <kattya.cuevas@gmail.com>
2014-10-09 12:25:38 -0500

    9.3 TDD index ROJO

--- 2014-10/04-nosql/devflix/spec/controllers/series_controller_spec.rb
+++ 2014-10/04-nosql/devflix/spec/controllers/series_controller_spec.rb
@@ -5,5 +5,12 @@ describe SeriesController do
       get :index
       expect(response.status).to eq 200
     end
+    it 'render json' do
+      Serie.create(nombre: 'Bates Motel')
+      get :index
+      expect(response.status).to eq 200
+      json = JSON.parse(response.body)
+      expect(json.size).to eq 1
+    end
   end
 end
