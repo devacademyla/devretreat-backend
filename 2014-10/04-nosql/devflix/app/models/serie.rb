d9bcddd8c87dee6566fbd6c192df6ce4237d0753
Kattya <kattya.cuevas@gmail.com>
2014-10-09 15:10:53 -0500

    13. Embeber Temporada en Serie

--- 2014-10/04-nosql/devflix/app/models/serie.rb
+++ 2014-10/04-nosql/devflix/app/models/serie.rb
@@ -1,4 +1,6 @@
 class Serie
   include Mongoid::Document
   field :nombre, type: String
+
+  embeds_many :temporadas
 end
--- 2014-10/04-nosql/devflix/app/models/temporada.rb
+++ 2014-10/04-nosql/devflix/app/models/temporada.rb
@@ -2,4 +2,6 @@ class Temporada
   include Mongoid::Document
   field :nombre, type: String
   field :capitulos, type: Integer
+
+  embedded_in :serie
 end
